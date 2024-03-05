055ab42adb0a1ae94294daff8cf77d5fb5422f49
Lautaro Hillkirk <hillkirklautaro18@gmail.com>
2024-03-05 10:21:41 -0300

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def read_root():
 
 @app.get("/redirect", include_in_schema=False)
 def redirect_to_docs():
-    link = "https://pi01-steam-mlops.onrender.com/"
+    link = "https://pi01-steam-mlops.onrender.com/docs"
     raise HTTPException(status_code=302, detail="Redirecting", headers={"Location": link})
 
 
